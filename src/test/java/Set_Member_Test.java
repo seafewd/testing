730e78534613a1e6435a8a3be1ba4620a670166c
alex <alexander.korpas@protonmail.com>
2021-11-15 20:33:31 +0100

    member() test

---
+++ src/test/java/Set_Member_Test.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Set_Member_Test {
+
+    Set set = new Set();
+
+    @BeforeEach
+    void setup() {
+        set.insert(7);
+        set.insert(-1);
+        set.insert(5);
+        set.insert(0);
+        set.insert(999);
+    }
+    // check whether an inserted element is in a list
+    @Test
+    void checkIsMember() {
+        assertTrue(set.member(5));
+    }
+
+    @Test
+    void checkIsNotMember() {
+        assertFalse(set.member(4));
+    }
+}
