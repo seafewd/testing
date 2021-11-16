e55de1f4247bc291e9e9ab5e1e2399ce4d822788
alex <alexander.korpas@protonmail.com>
2021-11-16 22:53:07 +0100

    added test skeletons for WorkSchedule

---
+++ src/test/java/WorkSchedule_NextIncomplete_Test.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkSchedule_NextIncomplete_Test {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void nextIncomplete() {
+    }
+}
