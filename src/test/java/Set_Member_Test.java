a5963bda845465d9d6812e75507e34ca98d42322
rob <dauda@dauda>
2021-11-17 11:28:18 +0100

    Restructured Set_Member_Test

--- src/test/java/Set_Member_Test.java
+++ src/test/java/Set_Member_Test.java
@@ -19,6 +19,45 @@ class Set_Member_Test {
         set.insert(0);
         set.insert(999);
     }
+
+
+    /**
+     * Statement coverage tests (and consequently branch coverage)
+     *
+     * Statement 1: the for loop
+     * Statement 2: 'if (n == x)'
+     * Statement 3: 'isMember = true'
+     * Statement 4: 'break'
+     * Statement 5: 'return isMember'
+     */
+
+
+
+    @Test
+    void statementCoverage(){
+
+        // Purpose: If the element is a member, we will enter the for loop
+        // and execute statement 3 and 4
+        // * Statement 1: the for loop
+        // * Statement 2: 'if (n == x)'
+        // * Statement 3: 'isMember = true'
+
+        checkIsMember();
+        checkIsMember2();
+
+        // Purpose: If the element is not a member, we will enter the for loop
+        // and execute statement 5
+        // * Statement 1: the for loop
+        // * Statement 5: 'if (n == x)'
+        checkIsNotMember();
+
+
+
+
+    }
+
+
+
     // check whether an element which we know is in the list,
     // is in the list
     @Test
